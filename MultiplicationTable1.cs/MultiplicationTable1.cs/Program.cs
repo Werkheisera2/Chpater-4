abaf76f54b4cc2711ad77c976a47174475135c9b
FrancisB1 <gaspump1112@gmail.com>
2014-09-18 08:40:04 -0400

    booty
    ya

---
+++ MultiplicationTable1.cs/MultiplicationTable1.cs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiplicationTable.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1 * 1;
+            int s = 2 * 2;
+            int d = 3 * 3;
+            int f = 4 * 4;
+            int g = 5 * 5;
+            int h = 6 * 6;
+            int j = 7 * 7;
+            int k = 8 * 8;
+            int l = 9 * 9;
+            int z = 10 * 10;
+
+            Console.WriteLine(" 1 x 1 = " + a );
+            Console.WriteLine(" 2 x 2 = " + s );
+            Console.WriteLine(" 3 x 3 = " + d );
+            Console.WriteLine(" 4 x 4 = " + f );
+            Console.WriteLine(" 5 x 5 = " + g );
+            Console.WriteLine(" 6 x 6 = " + h);
+            Console.WriteLine(" 7 x 7 = " + j);
+            Console.WriteLine(" 8 x 8 = " + k);
+            Console.WriteLine(" 9 x 9 = " + l);
+            Console.WriteLine(" 10 x 10 = " + z);
+            
+        }
+    }
+}
